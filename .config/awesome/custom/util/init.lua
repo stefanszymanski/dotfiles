297d82b6d3eb87031bd4223e9fce04ea2cad6eb0
Stefan Szymanski <stefan.szymanski@posteo.de>
2020-05-17 18:28:57 +0200

    awesomewm: remove useless code line

--- .config/awesome/custom/util/init.lua
+++ .config/awesome/custom/util/init.lua
@@ -2,6 +2,5 @@ local wrequire     = require("lain.helpers").wrequire
 local setmetatable = setmetatable
 
 local util = { _NAME = "custom.util" }
-local tag = { _NAME = "custom.tag" }
 
 return setmetatable(util, { __index = wrequire })
