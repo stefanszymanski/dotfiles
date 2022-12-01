dbce210b61bf8c7e7b31b77da4446d76aa560149
Stefan Szymanski <stefan.szymanski@posteo.de>
2022-12-01 14:46:37 +0100

    nvim: disable clipboard sync of tmux plugin

--- .config/nvim/lua/plugin-settings/tmux.lua
+++ .config/nvim/lua/plugin-settings/tmux.lua
@@ -11,6 +11,9 @@ M.config = function()
         resize = {
             resize_step_x = 5,
             resize_step_y = 5,
+        },
+        copy_sync = {
+            enable = false,
         }
     }
 
