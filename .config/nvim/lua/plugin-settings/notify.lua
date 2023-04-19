eb1d69b0495faa4b05f3f532592991f70836f409
Stefan Szymanski <stefan.szymanski@posteo.de>
2023-04-19 09:52:07 +0200

    nvim: improve notification settings

--- .config/nvim/lua/plugin-settings/notify.lua
+++ .config/nvim/lua/plugin-settings/notify.lua
@@ -8,6 +8,11 @@ M.config = function()
     notify.setup {
         stages = 'static',
         render = 'minimal',
+        level = vim.log.levels.WARN,
+        timeout = 3000,
+        minimum_width = 20,
+        max_width = 160,
+        max_height = 6,
     }
 
     telescope.load_extension('notify')
