95701cbb5639a6367d97708ef7987ba66324539f
Stefan Szymanski <stefan.szymanski@posteo.de>
2023-04-24 20:48:21 +0200

    nvim: add peek mapping

--- .config/nvim/lua/plugins/peek.lua
+++ .config/nvim/lua/plugins/peek.lua
@@ -6,6 +6,25 @@ local M = {
 
 M.config = function()
     require('peek').setup()
+    -- Register <leader>P mapping only for markdown files
+    vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'markdown',
+        callback = function()
+            vim.api.nvim_buf_set_keymap(0, 'n', '<leader>P', "", {
+                callback = function()
+                    local peek = require('peek')
+                    if peek.is_open() then
+                        peek.close()
+                    else
+                        peek.open()
+                    end
+                end,
+                desc = 'Markdown preview',
+                noremap = true,
+                silent = true
+            })
+        end
+    })
 end
 
 return M
