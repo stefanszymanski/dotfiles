0bd42c8529f795f0d330488da79304cb1dd7f971
Stefan Szymanski <stefan.szymanski@posteo.de>
2023-05-04 19:37:42 +0200

    nvim: add copilot bindings

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -7,12 +7,18 @@ local M = {
             dependencies = 'hrsh7th/nvim-cmp',
         },
     },
+    keys = {
+        { '<leader>pe', '<cmd>Copilot enable<CR>', desc = 'Enable' },
+        { '<leader>pd', '<cmd>Copilot disable<CR>', desc = 'Disable' },
+        { '<leader>ps', '<cmd>Copilot status<CR>', desc = 'Status' },
+        { '<leader>pp', '<cmd>Copilot panel<CR>', desc = 'Panel' },
+    },
 }
 
 M.config = function()
     require('copilot').setup({
         suggestion = { enabled = false },
-        panel = { enabled = false },
+        panel = { enabled = true },
     })
     require('copilot_cmp').setup({
         formatters = {
